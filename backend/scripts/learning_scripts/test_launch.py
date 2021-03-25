d29b2cf67d62c37771b52a281f239c5f33167989
AndreiMitriakov <andrei.mitriakov@gmail.com>
2021-03-25 23:35:05 +0100

    test script stream logger

--- backend/scripts/learning_scripts/test_launch.py
+++ backend/scripts/learning_scripts/test_launch.py
@@ -2,19 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import logging
 import os
-import sys
 import rospy
-from base_logger import BaseLogger
+from utils.stream_logger import StreamLogger
+import datetime
 from std_msgs.msg import String
-import importlib
 
 
-class Learner(BaseLogger):
+class Learner:
     def __init__(self, port):
-        super(Learner, self).__init__(fname="log_"+str(port), logger_name="CENTRAL")
+        self.stream_logger = StreamLogger(port)
         self.port = port
+        self.log = lambda s: print(datetime.datetime.now(), s)
         os.environ["ROS_MASTER_URI"] = "http://localhost:" + str(self.port)
         rospy.init_node("example")
         self.p = rospy.Publisher("endless", String)
@@ -22,17 +21,21 @@ class Learner(BaseLogger):
     def learn(self):
         cnt = 0
         while not rospy.is_shutdown():
-            self.logger.info("Iteration on"+str(self.port))
+            self.log("Iteration on"+str(self.port))
             self.p.publish(String(data=str(cnt)))
             cnt += 1
             rospy.sleep(1)
             if cnt > 2:
-                self.logger.info("done")
+                self.log("done")
                 break
 
+    def __del__(self):
+        self.stream_logger.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', type=int, default=11311, help='ROS MASTER URI port')
     args = parser.parse_args()
     Learner(args.p).learn()
+
+
