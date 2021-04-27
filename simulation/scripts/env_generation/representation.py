a2465dddf394826f166247dff745e7958bd3f7e3
gwaxG <andrei.mitriakov@gmail.com>
2021-04-27 10:00:42 +0200

    gaussian env sampling fix

--- simulation/scripts/env_generation/representation.py
+++ simulation/scripts/env_generation/representation.py
@@ -235,14 +235,12 @@ class StairFloor(Group):
         dh = StairFloor.dimensions["height"]["max"] - StairFloor.dimensions["height"]["min"]
         mu_h = dh * eps + StairFloor.dimensions["height"]["min"]
         sigma_h = dh * eps
-        print(sigma_n, sigma_l, sigma_h)
         # clip a sampled value to make it fitting in its min-max interval
-        step_n = int(np.clip(
-            # np.round is to put a float value to int
-            np.round(np.random.normal(mu_n, sigma_n)),
+        step_n = np.clip(
+            int(np.round(np.random.normal(mu_n, sigma_n))),
             StairFloor.dimensions["n"]["min"],
             StairFloor.dimensions["n"]["max"]
-        ))
+        )
         length = np.clip(
             np.random.normal(mu_l, sigma_l),
             StairFloor.dimensions["length"]["min"],
