82f818c8f21d6c3c6a91e89c15c561a8b013325c
gwaxG <andrei.mitriakov@gmail.com>
2021-04-27 10:50:31 +0200

    database  node message definition fix

--- backend/go.mod
+++ backend/go.mod
@@ -3,9 +3,8 @@ module github.com/gwaxG/robot_ws/backend
 go 1.16
 
 require (
-	github.com/aler9/goroslib v0.0.0-20210403160619-c4f43e6c0bff
+	github.com/aler9/goroslib v0.0.0-20210415211335-70a739e3d6b4
 	github.com/fatih/color v1.10.0
-	github.com/gin-gonic/gin v1.7.0
-	github.com/gwaxG/robot_ws/monitor v0.0.0-20210407210029-aee2efc2b1bf
+	github.com/gin-gonic/gin v1.7.1
 	go.mongodb.org/mongo-driver v1.5.1
 )
--- backend/pkg/common/structs.go
+++ backend/pkg/common/structs.go
@@ -8,14 +8,15 @@ type RolloutAnalytics struct {
 	ExpSeries   string  `bson:"exp_series" json:"exp_series"`
 	Experiment  string  `bson:"experiment" json:"experiment"`
 	Seq         int32   `bson:"seq" json:"seq"`
-	Sensors     string  `bson:"Sensors" json:"Sensors"`
+	Sensors     string  `bson:"sensors" json:"sensors"`
 	Arm         bool    `bson:"arm" json:"arm"`
 	Angular     bool    `bson:"angular" json:"angular"`
 	Progress    float32 `bson:"progress" json:"progress"`
 	Reward      float32 `bson:"reward" json:"reward"`
-	AngularM    float32 `bson:"angularm" json:"angularm"`
+	AngularM    float32 `bson:"angular_m" json:"angular_m"`
 	Deviation   float32 `bson:"deviation" json:"deviation"`
 	Accidents   string  `bson:"accidents" json:"accidents"`
 	TimeSteps   int32   `bson:"time_steps" json:"time_steps"`
 	Log         string  `bson:"log" json:"log"`
+	Debug       float32 `bson:"debug" json:"debug"`
 }
